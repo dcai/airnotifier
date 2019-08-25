cb586fa69052a6e65180bccde490e433b195c32a
Dongsheng Cai <d@tux.im>
2019-08-25 13:38:06 +0000

    admin.py wip

---
+++ admin.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import click
+import sys
+
+
+@click.group(name="admin")
+def cli():
+    pass
+
+
+@cli.command(name="adduser")
+@click.option("--email", help="email")
+@click.option("--password", help="org id")
+@click.option("--org", help="org id")
+def cmd_add_user(email, password, org):
+    print("add user")
+
+
+@cli.command(name="chanageorg")
+@click.option("--email", help="email")
+@click.option("--org", help="org id")
+def cmd_change_org(user, org):
+    pass
+
+
+@cli.command(name="changepassword")
+@click.option("--email", help="email")
+@click.option("--password", help="password")
+def cmd_chanage_password(user, password):
+    pass
+
+
+if __name__ == "__main__":
+    cli()
