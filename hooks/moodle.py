720f0572f3e4c222e183842c6785e6d2b650582f
Dongsheng Cai <d@tux.im>
2019-12-15 02:56:48 +0000

    fix(moodle): update moodle hooks

--- hooks/moodle.py
+++ hooks/moodle.py
@@ -1,19 +1,30 @@
 import requests
 from api import API_PERMISSIONS
 
-HUBURL = "http://moodle.net/local/sitecheck/check.php"
+HUBURL = "https://moodle.net/local/sitecheck/check.php"
 
 
 def process_pushnotification_payload(data):
     extra = data.get("extra", {})
-    userfrom = extra.get("userfrom", None)
+    userfrom = extra.get("userfromfullname", None)
+    site = extra.get("site", None)
     timecreated = extra.get("timecreated", None)
-    userto = extra.get("userto", None)
-    subject = extra.get("subject", None)
-    fullmessage = extra.get("fullmessage", None)
+    message = extra.get("smallmessage", None)
+    notif = extra.get("notification", None)
+    title = extra.get("sitefullname", None)
+
+    if not message:
+        message = extra.get("fullmessage", None)
+
+    if not title:
+        title = "Notification"
+
+    data["fcm"] = {
+        "data": {"title": title, "site": site, "userfrom": userfrom, "notif": notif}
+    }
 
     if "alert" not in data:
-        data["alert"] = fullmessage
+        data["alert"] = message
 
     if not "wns" in extra:
         data["extra"]["wns"] = {
@@ -21,9 +32,6 @@ def process_pushnotification_payload(data):
             "template": "ToastText01",
             "text": [data["alert"]],
         }
-
-    if not "fcm" in extra:
-        data["extra"]["fcm"] = {"fcm-message": "this is fcm hook"}
 
     return data
 
@@ -45,3 +53,7 @@ def process_accesskey_payload(data):
             | API_PERMISSIONS["send_broadcast"][0]
         )
         return data
+
+
+def process_token_payload(data):
+    return data
